c3c0946b3dbb3018851a2ecf3293e12e6ab57067
Gert Thiel <GertThiel@gmx.net>
2010-11-25 09:26:50 +0100

    Added Bryan's patch 0001-873-simple-hack-fix-for-chained-scopes.patch

---
+++ config/initializers/0001-873-simple-hack-fix-for-chained-scopes.patch.rb
@@ -0,0 +1,29 @@
+module ActiveRecord
+  module NamedScope
+    class Scope
+
+      delegate :member_class, :to => :proxy_found
+
+      include Hobo::Scopes::ApplyScopes
+
+      def respond_to?(method, include_private=false)
+        super || scopes.include?(method) || proxy_scope.respond_to?(method, include_private)
+      end
+      
+      private
+      
+      def method_missing(method, *args, &block)
+        if respond_to?(method) && scopes.include?(method)
+          scopes[method].call(self, *args)
+        elsif method==:scoped
+          Scope.new(self, args[0], &block)
+        else
+          with_scope :find => proxy_options do
+            proxy_scope.send(method, *args, &block)
+          end
+        end
+      end
+
+    end
+  end
+end

50ed456e0969df59a521575c60f8d8a12c2e9366
Gert Thiel <GertThiel@gmx.net>
2010-11-25 09:28:33 +0100

    Updated the Transaction model and the chained scopes spec to demonstrate that Bryan's fix does not yet fix the bug completely

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,2 +1,20 @@
 class Translation < ActiveRecord::Base
+
+  # These method shall mimik I18n::Backend::ActiveRecord::Translation
+  # to demonstrate that 0001-873-simple-hack-fix-for-chained-scopes.patch
+  # does not completely fix the chained scopes bug
+  #
+  # http://groups.google.com/group/hobousers/browse_thread/thread/b18aa93951df0a0c
+  # https://hobo.lighthouseapp.com/projects/8324-hobo/tickets/873-chained-scopes-break-with-hobo-rails-239
+  #
+  class << self
+    def locale(locale)
+      scoped(:conditions => { :locale => locale.to_s })
+    end
+
+    def key(key)
+      scoped(:conditions => { :key => key })
+    end
+  end
+
 end
--- spec/models/chained_scopes_spec.rb
+++ spec/models/chained_scopes_spec.rb
@@ -7,7 +7,19 @@ describe Translation do
     Translation.count.should == 6
   end
 
+  # I applied 0001-873-simple-hack-fix-for-chained-scopes.patch with an
+  # initializer in
+  # RAILS_ROOT/config/initializers/0001-873-simple-hack-fix-for-chained-scopes.patch.rb
+
+  # 0001-873-simple-hack-fix-for-chained-scopes.patch fixes this:
+  #
   it "chained scopes should return correct results" do
     Translation.scoped({ :conditions => {:locale => :de.to_s} }).scoped({ :conditions => {:key => 'first'} }).count.should == 1
+  end
+
+  # 0001-873-simple-hack-fix-for-chained-scopes.patch DOES NOT fix this:
+  #
+  it "chained scopes should return correct results (2)" do
+    Translation.locale(:de).key('first').count.should == 1
   end
 end
